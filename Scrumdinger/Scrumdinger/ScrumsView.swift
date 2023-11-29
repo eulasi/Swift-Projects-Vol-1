0272b06ab1226f345d1cdff7d989e825561cd4a3
Emmanuel <96310496+eulasi@users.noreply.github.com>
2023-11-29 12:11:15 -0500

    Navigation Stack Added
    NavigationStack added. iOS 16.0 adapted

--- Scrumdinger/Scrumdinger/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -1,22 +1,26 @@
-//
-//  ScrumsView.swift
-//  Scrumdinger
-//
-//  Created by A1465 on 11/25/23.
-//
-
 import SwiftUI
 
 struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
-                .listRowBackground(scrum.theme.mainColor)
+        NavigationStack {
+            List(scrums) { scrum in
+                    NavigationLink(destination: Text(scrum.title)) {
+                        CardView(scrum: scrum)
+                    }
+                    .listRowBackground(scrum.theme.mainColor)
+                }
+                .navigationTitle("Daily Scrums")
+                .toolbar {
+                    Button(action:{}) {
+                        Image(systemName: "plus")
+                    }
+                    .accessibilityLabel("New Scrum")
+                }
+            }
         }
     }
-}
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
